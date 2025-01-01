c572a31f26ddcea447eda7aeda39ad541b573e53
Shivang <shivangnagpure66@gmail.com>
2025-01-01 17:24:48 +0530

    Practice_01

---
+++ src/Practice_for_input.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Practice_for_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       /* int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a);
+        System.out.println(b);
+
+
+
+
+        int sum=a+b;
+        System.out.println(sum);
+
+
+        */
+
+
+
+        String name = sc.nextLine();
+        System.out.println(name);
+
+    }
+}
