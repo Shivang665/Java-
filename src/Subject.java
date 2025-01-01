d0a44d2ea14109f6466d114f49d61fd914df4b7f
Shivang <shivangnagpure66@gmail.com>
2025-01-01 20:02:10 +0530

    Subject

---
+++ src/Subject.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Subject{
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int i,j,n;
+        n = 4;
+        for(i=0;i<n;i++) {
+            for (j = 0; j < n; j++) {
+
+                System.out.print(" "+"*"+" ");
+            }
+            System.out.println(" ");
+
+        }
+    }
+}
