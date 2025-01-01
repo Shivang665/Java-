2480066bb67ac40e71a5f1aa4acdb3983b6d6432
Shivang <shivangnagpure66@gmail.com>
2025-01-01 17:07:14 +0530

    Hello World

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+package src;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int a=20;
+        int b=30;
+        int sum = a+b;
+        int multiply=a*b;
+        System.out.println(sum);
+        System.out.println(multiply);
+    }
+}
+
