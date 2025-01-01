15188036a8f6d7fbefc4fb94adc92b075a0cb6fa
Shivang <shivangnagpure66@gmail.com>
2025-01-01 17:18:54 +0530

    Name

---
+++ src/Name.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+public class Name{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Hey !! there what's your name :-");
+        String name = sc.nextLine();
+        System.out.println("My name is "+name);
+
+
+
+
+        String yourName="Ram";
+        System.out.println(yourName);
+    }
+}
