7365d2388261aa57e0977db804b56e94b53d1ddc
Shivang <shivangnagpure66@gmail.com>
2025-01-01 20:08:32 +0530

    Switch case

---
+++ src/pract_01.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class pract_01{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int button = sc.nextInt();
+
+
+        switch(button)
+        {
+            case 1:
+                System.out.println("Hello");
+                break;
+            case 2:
+                System.out.println("Hey !!!");
+                break;
+            case 3:
+                System.out.println("Bonjure");
+                break;
+            default:
+                System.out.println("Invalid button");
+        }
+    }
+}
+
